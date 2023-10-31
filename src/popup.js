d8c6d10697193be4efc527bad097935f784044d7
Paulo Lima <limapaulobsb@gmail.com>
2023-10-31 15:20:57 -0300

    Create scrollable main element that will contain the two sections 'view' and 'code'

--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,15 @@
-console.log('Test');
+const mainContainer = document.querySelector('main');
+const viewButton = document.getElementById('view-button');
+const codeButton = document.getElementById('code-button');
+
+viewButton.addEventListener('click', () => {
+  mainContainer.scrollTo(0, 0);
+  codeButton.classList.remove('selected');
+  viewButton.classList.add('selected');
+});
+
+codeButton.addEventListener('click', () => {
+  mainContainer.scrollTo(400, 0);
+  viewButton.classList.remove('selected');
+  codeButton.classList.add('selected');
+});
